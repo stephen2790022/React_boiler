5c2772dc44bacfbb8fc4c4f5b4b20f15129d39e3
stephen2790022 <toulouse.stephen1@gmail.com>
2021-06-02 18:57:43 +0200

    react app basic init / basic redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const App = () => {
+    return (
+        <Switch>
+            <Route path='/' component={Home}/>
+        </Switch>
+    );
+};
+
+export default App;
---
+++ src/reduxe/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    userData: []
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'value':
+            break;
+    
+        default:
+            break;
+    }
+}
+
